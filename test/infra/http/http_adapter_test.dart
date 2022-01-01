d7c006afe23e47b4c49502a9789fbde6c0b14ec3
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-01-01 17:45:31 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -91,5 +91,13 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return ServerError if post returns 500', () async {
+      mockResponse(500);
+      //se for exceção, a gente coloca future, se não, é response
+      final future = await sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
